349fddd265de513f203e4e1b25db2b1230feb317
Mitja Karhusaari <mitja@karhusaari.me>
2020-11-29 11:14:21 +0200

    Write cucumber tests

--- src/test/java/ohtu/Stepdefs.java
+++ src/test/java/ohtu/Stepdefs.java
@@ -1,43 +1,133 @@
 package ohtu;
 
 import io.cucumber.java.Before;
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+
+import static org.junit.Assert.*;
+import ohtu.ui.TextUI;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import ohtu.io.StubIO;
 import ohtu.storage.InMemorySuggestionDao;
-import static org.junit.Assert.*;
-import ohtu.ui.TextUI;
+import ohtu.domain.BookSuggestion;
+import ohtu.domain.Suggestion;
+import ohtu.domain.SuggestionVisitor;
+import ohtu.domain.SuggestionFieldValue;
 
 public class Stepdefs {
-
-    StubIO io;
-    List<String> inputLines;
+    volatile StubIO io;
     InMemorySuggestionDao memoryDao;
+    TextUI ui;
+    Thread uiThread;
 
     @Before
     public void setup() {
-
         memoryDao = new InMemorySuggestionDao();
-        inputLines = new ArrayList<>();
-
+        io = new StubIO();
+        ui = new TextUI(io, memoryDao);
+        uiThread = new Thread(ui::launch);
+        uiThread.start();
     }
 
     @Given("command new is selected$")
-    public void commandNewSelected() throws Throwable {
-        inputLines.add("new");
+    public void commandNewSelected() {
+        io.input("new");
+        io.runUntil(line -> line.equals("> "));
+    }
 
-        io = new StubIO(inputLines);
-        TextUI kayttoliittyma = new TextUI(io, memoryDao);
-        kayttoliittyma.launch();
+    @Given("command edit is selected")
+    public void commandEditSelected() {
+        io.input("edit");
+        io.runUntil(line -> line.equals("> "));
+    }
+
+    @Given("new book suggestion is created with title {string}, author {string} and isbn {string}")
+    public void newBookSuggestionIsCreated(String title, String author, String isbn) {
+        BookSuggestion s = new BookSuggestion();
+        s.setTitle(title);
+        s.setAuthor(author);
+        s.setIsbn(isbn);
+        memoryDao.saveSuggestion(s);
+    }
+
+    @When("user inputs an invalid suggestion id {int}")
+    public void userInputsAnInvalidSuggestionId(int id) {
+        io.input(String.valueOf(id));
+    }
+
+    @When("user inputs a valid suggestion id {int}")
+    public void userInputsAValidSuggestionId(int id) {
+        io.input(String.valueOf(id));
+    }
+
+    @When("user inputs a new title {string}")
+    public void userInputsANewTitle(String title) {
+        io.trigger("Title:", title);
+    }
+
+    @When("user inputs a new author {string}")
+    public void userInputsANewAuthor(String author) {
+        io.trigger("Author:", author);
+    }
+
+    @When("user inputs a new isbn {string}")
+    public void userInputsANewISBN(String isbn) {
+        io.trigger("ISBN:", isbn);
+    }
+
+    @When("user leaves the title unmodified")
+    public void userLeavesTheTitleUnmodified() {
+        io.trigger("Title:", null);
+    }
+
+    @When("user leaves the author unmodified")
+    public void userLeavesTheAuthorUnmodified() {
+        io.trigger("Author:", null);
+    }
+
+    @When("user leaves the isbn unmodified")
+    public void userLeavesTheISBNUnmodified() {
+        io.trigger("ISBN:", null);
     }
     
-    @Then("system will respond with {string}")
+    @Then("system will respond with line {string}")
     public void systemWillRespondWith(String expectedOutput) {
-        assertTrue(io.getPrints().contains(expectedOutput));
+        assertTrue(io.runUntil(expectedOutput::equals));
     }
 
+    @Then("system will respond with line containing {string}")
+    public void systemWillRespondWithLineContaining(String segment) {
+        assertTrue(io.runUntil(line -> line.contains(segment)));
+    }
+
+    @Then("system will show the command prompt")
+    public void systemWillShowTheCommandPrompt() {
+        assertTrue(io.runUntil(line -> line.equals("> ")));
+    }
+
+    @Then("field {string} of suggestion {int} has value of {string}")
+    public void fieldOfSuggestionHasValueOf(String fieldName, int id, String value) {
+        Suggestion suggestion = memoryDao.getSuggestionById(id);
+
+        assertFalse(suggestion == null);
+
+        boolean[] visited = { false };
+
+        suggestion.visit(new SuggestionVisitor() {
+            @Override
+            public void visitString(SuggestionFieldValue<String> field) {
+                if (field.getName().equals(fieldName)) {
+                    assertEquals(value, field.getValue());
+                    visited[0] = true;
+                }
+            }
+        });
+
+        assertTrue(visited[0]);
+    }
 }
---
+++ src/test/resources/ohtu/editsuggestion.feature
@@ -0,0 +1,34 @@
+Feature: käyttäjä voi muokata olemassaolevaa vinkkiä
+
+    Scenario: käyttäjä syöttää virheellisen vinkin numeron
+
+        Given new book suggestion is created with title "title", author "author" and isbn "isbn"
+        And   command edit is selected
+        When  user inputs an invalid suggestion id 123
+        Then  system will respond with line containing "Invalid ID: 123"
+
+    Scenario: käyttäjä valitsee onnistuneesti vinkin ja syöttää uudet arvot
+
+        Given new book suggestion is created with title "title", author "author" and isbn "isbn"
+        And   command edit is selected
+        When  user inputs a valid suggestion id 0
+        And   user inputs a new title "new title"
+        And   user inputs a new author "new author"
+        And   user inputs a new isbn "new isbn"
+        Then  system will show the command prompt
+        And   field "title" of suggestion 0 has value of "new title"
+        And   field "author" of suggestion 0 has value of "new author"
+        And   field "isbn" of suggestion 0 has value of "new isbn"
+
+    Scenario: käyttäjä valitsee onnistuneesti vinkin mutta jättää arvot ennalleen
+
+        Given new book suggestion is created with title "title", author "author" and isbn "isbn"
+        And   command edit is selected
+        When  user inputs a valid suggestion id 0
+        And   user leaves the title unmodified
+        And   user leaves the author unmodified
+        And   user leaves the isbn unmodified
+        Then  system will show the command prompt
+        And   field "title" of suggestion 0 has value of "title"
+        And   field "author" of suggestion 0 has value of "author"
+        And   field "isbn" of suggestion 0 has value of "isbn"
